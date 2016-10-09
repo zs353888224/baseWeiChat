1ae30365c4268b864237af9098f817ce38ac971f
张帅 <shuai.zhang@monstar-lab-cn.com>
2016-10-09 13:48:41 +0800

    微信通讯的组件基本完成，未处理部分根据实际业务进行开发

--- baseWeichat-domain/src/main/java/com/wscq/baseWeiChat/domain/constants/SystemConstants.java
+++ baseWeichat-domain/src/main/java/com/wscq/baseWeiChat/domain/constants/SystemConstants.java
@@ -2,17 +2,19 @@ package com.wscq.baseWeiChat.domain.constants;
 
 /**
  * Like the name
- *
+ * <p>
  * Created by zs on 16/9/29.
  */
 public class SystemConstants {
 
     /**
-     * redis 全局变量
+     * redis hash全局变量
      */
     public static final String APP_GLOBAL_VARIABLE = "APP_GLOBAL_VARIABLE";
 
+    /**
+     * 存储access_token关键字
+     */
     public static final String WEICHAT_ACCESS_TOKEN = "WEICHAT_ACCESS_TOKEN";
 
-    public static final String WECHAT_ACCESS_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token";
 }
